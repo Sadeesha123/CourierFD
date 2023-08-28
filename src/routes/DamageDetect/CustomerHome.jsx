538f2499f9c1dc86a1bef177f2fa5adf27f90db5
Randimal Fernando <randimalfdo@gmail.com>
2023-08-28 21:37:30 +0530

    scan qr side panel

--- src/routes/DamageDetect/CustomerHome.jsx
+++ src/routes/DamageDetect/CustomerHome.jsx
@@ -21,7 +21,7 @@ function CustomerHome() {
           <div className="common-body p-5 flex flex-col h-full items-center justify-center">
             <div className="form-body w-[80%] flex flex-row p-5 mx-auto items-center justify-center space-x-5">
               
-              <Link to="/ScanQR"><div className="option-card p-5 w-[420px] border-[1px] border-gray-200 text-center rounded-md bg-white shadow-md shadow-slate-300">
+              <a href="/ScanQR"><div className="option-card p-5 w-[420px] border-[1px] border-gray-200 text-center rounded-md bg-white shadow-md shadow-slate-300">
                   <div className="card-body ">
                       <div className="img-box">
                         <img src={qr} alt="" srcset="" className="object-cover w-full h-full"/>
@@ -30,10 +30,10 @@ function CustomerHome() {
                             <h2 className="fonr-semibold text-gray-900">Scan QR now</h2>
                       </div>
                   </div>
-              </div></Link>
+              </div></a>
 
 
-              <Link to="/Inquiry"><div className="option-card p-5 w-[420px] border-[1px] border-gray-200 text-center rounded-md bg-white shadow-md shadow-slate-300">
+              <a href="/Inquiry"><div className="option-card p-5 w-[420px] border-[1px] border-gray-200 text-center rounded-md bg-white shadow-md shadow-slate-300">
                   <div className="card-body ">
                       <div className="img-box">
                         <img src={inq} alt="" srcset="" className="object-cover w-full h-full"/>
@@ -42,7 +42,7 @@ function CustomerHome() {
                             <h2 className="fonr-semibold text-gray-900">Inquiry</h2>
                       </div>
                   </div>
-              </div></Link>
+              </div></a>
 
 
 
