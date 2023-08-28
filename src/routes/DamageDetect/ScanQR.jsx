b0f675d15c6fd8e647d2022f24c1a2bd0ad525dc
Randimal Fernando <randimalfdo@gmail.com>
2023-08-28 11:41:06 +0530

    scan qr bg updated

--- src/routes/DamageDetect/ScanQR.jsx
+++ src/routes/DamageDetect/ScanQR.jsx
@@ -5,6 +5,7 @@ import { faPlus, faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
 import Sidepanel from "../../components/sidepanel";
 import qr from './images/qr.jpg';
 import inq from './images/inq.jpg';
+import bg from '../../images/mainbg1.jpg';
 
 function ScanQR() {
   const [stream, setStream] = useState(null);
@@ -33,11 +34,13 @@ function ScanQR() {
 
 
 
+
   return (
     <div className="main-body h-screen w-full bg-slate-100">
+       <img src={bg} alt="" srcset="" className="object-cover w-[100%] h-[100%] fixed" />
       <div className="main-body-container h-screen w-full flex flex-row">
         <Sidepanel />
-        <div className="w-5/6 side-panel bg-slate-100 p-5">
+        <div className="w-5/6 side-panel p-5 md:ml-[300px] ml-16">
           <div className="common-body p-5 flex flex-col h-full items-center justify-center">
             <div className="form-body w-[80%] flex flex-row p-5 mx-auto items-center justify-center space-x-5">
               <div className="relative">
