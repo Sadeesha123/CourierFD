d005066406c83daa193f116c5404ffd76d601dd7
Shyamin Ayesh <me@shyamin.com>
2023-09-04 21:31:56 +0530

    conditional render timeline

--- src/pages/Tickets/SingleTicket.jsx
+++ src/pages/Tickets/SingleTicket.jsx
@@ -56,6 +56,38 @@ export default function SingleTicket() {
     return date.getHours() + ":" + date.getMinutes()
   }
 
+  // RENDER TIMELINE
+  function TimelineItems() {
+    if ( ticket.timeline != null ) {
+      return ticket.timeline.map((item) => {
+        return (
+          <div className="flex items-center w-full my-6 -ml-1.5">
+            <div className="w-1/12 z-10">
+              <div className="w-3.5 h-3.5 bg-blue-600 rounded-full"></div>
+            </div>
+            <div className="w-11/12">
+              <p className="text-sm">{ item.description }</p>
+              <p className="text-xs text-gray-500">{ convert_time(item.time.created)}</p>
+            </div>
+          </div>
+        )
+      })
+    } else {
+      return (
+        <div className="flex items-center w-full my-6 -ml-1.5">
+          <div className="w-1/12 z-10">
+            <div className="w-3.5 h-3.5 bg-blue-600 rounded-full"></div>
+          </div>
+          <div className="w-11/12">
+            <p className="text-sm">Ticket created</p>
+            <p className="text-xs text-gray-500">{ convert_time(ticket.time.created)}</p>
+          </div>
+        </div>
+      )
+    }
+  }
+
+
   if (loading) {
     return "Loading ..."
   } else {
@@ -122,7 +154,8 @@ export default function SingleTicket() {
                   <div className="flex-1 bg-white rounded-lg shadow-xl mt-4 p-8 ">
                     <h4 className="text-xl text-gray-900 font-bold">Time Line</h4>
                     <div className="relative px-4">
-                       { ticket.timeline.map(d => {
+                      <TimelineItems />
+                       {/* { ticket.timeline.map(d => {
                         return(
                           <>                            
                             <div className="flex items-center w-full my-6 -ml-1.5">
@@ -136,7 +169,7 @@ export default function SingleTicket() {
                             </div>
                           </>
                         )
-                       }) }
+                       }) } */}
                       
                     </div>
                   </div>
