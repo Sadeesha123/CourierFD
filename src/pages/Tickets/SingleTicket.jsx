390fcc4a13ec1b2dbf3dde1e5c49b779f69a8f45
Shyamin Ayesh <me@shyamin.com>
2023-09-04 21:36:37 +0530

    display timeline items

--- src/pages/Tickets/SingleTicket.jsx
+++ src/pages/Tickets/SingleTicket.jsx
@@ -72,18 +72,6 @@ export default function SingleTicket() {
           </div>
         )
       })
-    } else {
-      return (
-        <div className="flex items-center w-full my-6 -ml-1.5">
-          <div className="w-1/12 z-10">
-            <div className="w-3.5 h-3.5 bg-blue-600 rounded-full"></div>
-          </div>
-          <div className="w-11/12">
-            <p className="text-sm">Ticket created</p>
-            <p className="text-xs text-gray-500">{ convert_time(ticket.time.created)}</p>
-          </div>
-        </div>
-      )
     }
   }
 
@@ -155,22 +143,6 @@ export default function SingleTicket() {
                     <h4 className="text-xl text-gray-900 font-bold">Time Line</h4>
                     <div className="relative px-4">
                       <TimelineItems />
-                       {/* { ticket.timeline.map(d => {
-                        return(
-                          <>                            
-                            <div className="flex items-center w-full my-6 -ml-1.5">
-                              <div className="w-1/12 z-10">
-                                <div className="w-3.5 h-3.5 bg-blue-600 rounded-full"></div>
-                              </div>
-                              <div className="w-11/12">
-                                <p className="text-sm">{d.description}</p>
-                                <p className="text-xs text-gray-500">{ convert_time(d.time.created)}</p>
-                              </div>
-                            </div>
-                          </>
-                        )
-                       }) } */}
-                      
                     </div>
                   </div>
                 </div>
